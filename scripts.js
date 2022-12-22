3d3f025c9b387c28495f58278e800f0ef9c83fdc
miccraig15 <miccraig15@gmail.com>
2022-12-22 11:30:49 -0600

    Completed Take Off Button commands

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    takeoff.addEventListener('click', event => {
+    let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (response) {
+            let newStatus = document.querySelector("#flightStatus");
+            let newBackground = document.querySelector("#shuttleBackground");
+            let shuttleHeight1 = document.querySelector("#spaceShuttleHeight");
+            newStatus.innerHTML = "Shuttle in flight.";
+            newBackground.style.backgroundColor = "blue";
+            shuttleHeight1.innerHTML = 10000;
+        }
+    });
+});

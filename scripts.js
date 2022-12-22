c2f557eba9ebb7f1de18699e94b11d93aaa25016
miccraig15 <miccraig15@gmail.com>
2022-12-22 11:50:49 -0600

    Completed Land Button Commands

--- scripts.js
+++ scripts.js
@@ -4,12 +4,21 @@ window.addEventListener("load", function() {
     takeoff.addEventListener('click', event => {
     let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if (response) {
-            let newStatus = document.querySelector("#flightStatus");
-            let newBackground = document.querySelector("#shuttleBackground");
+            let newTakeOffStatus = document.querySelector("#flightStatus");
+            let newTakeOffBackground = document.querySelector("#shuttleBackground");
             let shuttleHeight1 = document.querySelector("#spaceShuttleHeight");
-            newStatus.innerHTML = "Shuttle in flight.";
-            newBackground.style.backgroundColor = "blue";
+            newTakeOffStatus.innerHTML = "Shuttle in flight.";
+            newTakeOffBackground.style.backgroundColor = "blue";
             shuttleHeight1.innerHTML = 10000;
         }
     });
+    landing.addEventListener('click', event => {
+    let response = window.alert("The shuttle is landing. Landing gear engaged.");
+            let newLandingStatus = document.querySelector("#flightStatus");
+            let newLandingShuttleBackground = document.querySelector("#shuttleBackground")
+            let newLandingShuttleHeight = document.querySelector("#spaceShuttleHeight")
+            newLandingStatus.innerHTML = "The shuttle has landed.";
+            newLandingShuttleBackground.style.backgroundColor = "green";
+            newLandingShuttleHeight.innerHTML = 0;
+    })
 });
